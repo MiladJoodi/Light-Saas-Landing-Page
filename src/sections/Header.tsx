4395678fddf304ae4ac06e53f958059b8f49b714
Milad Joodi <winbeta.com@gmail.com>
2024-08-13 23:15:18 +0330

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,7 +6,8 @@ import Image from "next/image";
 export const Header = () => {
   return (
     <header className="sticky top-0">
-      <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+        <p className="text-white/60 hidden md:block">Streamline your workflow and boost your productivity</p>
         <div className="inline-flex gap-1 items-center">
           <p>Get started for free</p>
           <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
@@ -25,7 +26,7 @@ export const Header = () => {
               <a href="#">Customers</a>
               <a href="#">Updates</a>
               <a href="#">Help</a>
-              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center">Get for free</button>
+              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">Get for free</button>
             </nav>
 
           </div>
