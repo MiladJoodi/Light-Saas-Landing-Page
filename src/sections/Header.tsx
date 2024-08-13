69ea021f3480fc9d388043df3269834eefc28c4b
Milad Joodi <winbeta.com@gmail.com>
2024-08-13 22:03:50 +0330

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -25,7 +25,7 @@ export const Header = () => {
               <a href="#">Customers</a>
               <a href="#">Updates</a>
               <a href="#">Help</a>
-              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium">Get for free</button>
+              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center">Get for free</button>
             </nav>
 
           </div>
