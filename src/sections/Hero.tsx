a48e0a4dab289a4153b5d591da82f709a60d6825
Milad Joodi <winbeta.com@gmail.com>
2024-08-15 15:58:22 +0330

    Hero Start

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,19 @@
 export const Hero = () => {
-  return null;
+  return (
+    <section>
+      <div className="container">
+        <div>
+          <div className="">Version 2.0 is here</div>
+          <h1>Pathway to productivity</h1>
+          <p>
+            Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your success.
+          </p>
+          <div>
+            <button>Get for free</button>
+            <button>Learn more</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
