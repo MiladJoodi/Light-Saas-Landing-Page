829239dfe3643e722de39b84adc069a7bf8c4946
Milad Joodi <winbeta.com@gmail.com>
2024-08-16 23:13:02 +0330

    up

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,9 +10,7 @@ export const Hero = () => {
       <div className="container">
         <div className="md:flex items-center">
           <div className="md:w-[478px]">
-            <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-lg tracking-tight">
-              Version 2.0 is here
-            </div>
+            <div className="tag">Version 2.0 is here</div>
             <h1 className="text-5xl md:text-7xl font-bold tracking-tigher bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-6">
               Pathway to productivity
             </h1>
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 
 export const ProductShowcase = () => {
   return (
-    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF]">
+    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
       <div className="container">
-        <div>Boost your productivity</div>
-        <h2>A more effective way to track progress</h2>
-        <p>
+        <div className="flex justify-center items-center">
+        <div className="tag">Boost your productivity</div>
+        </div>
+        <h2 className="text-center text-3xl font-bold tracking-tigher bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text">A more effective way to track progress</h2>
+        <p className="text-center">
           Effortlessly turn your ideas into a fully functional, response, SaaS website in just minutes with this template.
         </p>
         <Image src={productImage} alt="product Image" className=""/>
