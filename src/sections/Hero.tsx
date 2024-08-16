5a546a72117f9318181985021b425c9bbd1e8de1
Milad Joodi <winbeta.com@gmail.com>
2024-08-16 16:13:14 +0330

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,17 +1,18 @@
 import ArrowIcon from "@/assets/arrow-right.svg";
 import cogImage from "@/assets/cog.png";
+import cylinderImage from "@/assets/cylinder.png";
 import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <section className="pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183ec2,#eaeefe_66%)]">
+    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183ec2,#eaeefe_100%)]">
       <div className="container">
         <div className="md:flex items-center">
           <div className="md:w-[478px]">
             <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-lg tracking-tight">
               Version 2.0 is here
             </div>
-            <h1 className="text-5xl font-bold tracking-tigher bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-6">
+            <h1 className="text-5xl md:text-7xl font-bold tracking-tigher bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-6">
               Pathway to productivity
             </h1>
             <p className="text-xl text-[#010d3e] tracking-tight mt-6">
@@ -28,7 +29,18 @@ export const Hero = () => {
           </div>
 
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <Image src={cogImage} alt="Cog image" className="md:absolute md:h-full md:w-auto md:max-w-none" />
+            <Image
+              src={cogImage}
+              alt="Cog image"
+              className="md:absolute md:h-full md:w-auto md:max-w-none"
+            />
+            <Image
+              src={cylinderImage}
+              alt="cylinder Image"
+              width={220}
+              height={220}
+              className="hidden md:block -top-8 -left-32 md:absolute"
+            />
           </div>
         </div>
       </div>
