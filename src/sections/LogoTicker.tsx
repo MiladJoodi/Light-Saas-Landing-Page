c79e4fdd2181af0c42209cf367bacdc2a77b8db5
Milad Joodi <winbeta.com@gmail.com>
2024-08-16 22:33:03 +0330

    up

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,14 +1,50 @@
 import acmeLogo from "@/assets/logo-acme.png";
 import quantumLogo from "@/assets/logo-quantum.png";
 import echoLogo from "@/assets/logo-echo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
 import pulseLogo from "@/assets/logo-pulse.png";
 import apexLogo from "@/assets/logo-apex.png";
-
+import Image from "next/image";
 
 export const LogoTicker = () => {
   return (
-    <div>
-      
+    <div className="py-8 md:py-12 bg-white">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image
+              src={acmeLogo}
+              alt="acme Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={quantumLogo}
+              alt="quantum Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={echoLogo}
+              alt="echo Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={celestialLogo}
+              alt="celestial Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={pulseLogo}
+              alt="pulse Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={apexLogo}
+              alt="apex Logo"
+              className="logo-ticker-image"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,17 @@
+import productImage from "@/assets/product-image.png"
+import Image from "next/image";
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF]">
+      <div className="container">
+        <div>Boost your productivity</div>
+        <h2>A more effective way to track progress</h2>
+        <p>
+          Effortlessly turn your ideas into a fully functional, response, SaaS website in just minutes with this template.
+        </p>
+        <Image src={productImage} alt="product Image" className=""/>
+      </div>
+    </section>
+  );
 };
