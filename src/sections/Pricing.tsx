ae4f29d99a27a42416177a53bb55374ae7d14171
Milad Joodi <winbeta.com@gmail.com>
2024-08-17 16:57:48 +0330

    up

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -51,5 +51,45 @@ const pricingTiers = [
 ];
 
 export const Pricing = () => {
-  return null;
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h2 className="section-title">Pricing</h2>
+        <p className="section-description mt-5">
+          Free forever. Upgrade for unlimited tasks, better security, and
+          exclusive features.
+        </p>
+
+        <div>
+          {pricingTiers.map(
+            ({
+              title,
+              monthlyPrice,
+              buttonText,
+              popular,
+              inverse,
+              features,
+            }) => (
+              <div key={title}>
+                <h3>{title}</h3>
+                <div>
+                  <span>${monthlyPrice}</span>
+                  <span>/month</span>
+                </div>
+                <button>{buttonText}</button>
+                <ul>
+                  {features.map((feature)=> (
+                    <li key={feature}>{feature}</li>
+                  ))}
+                </ul>
+              </div>
+            )
+          )}
+          <div>
+            <h3></h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -11,10 +11,10 @@ export const ProductShowcase = () => {
           <div className="flex justify-center items-center">
             <div className="tag">Boost your productivity</div>
           </div>
-          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tigher bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-5">
+          <h2 className="section-title mt-5">
             A more effective way to track progress
           </h2>
-          <p className="text-center text-[22px] leading-[30px]tracking-tight text-[#010d3e] mt-5">
+          <p className="section-description mt-5">
             Effortlessly turn your ideas into a fully functional, response, SaaS
             website in just minutes with this template.
           </p>
