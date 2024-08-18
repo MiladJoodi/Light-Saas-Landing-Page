af3c6d8d32841206fba6a205fb3202fd6f98e586
Milad Joodi <winbeta.com@gmail.com>
2024-08-18 19:44:45 +0330

    Update Footer.tsx

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,36 @@
+import logo from "@/assets/logosaas.png";
+import SocialX from "@/assets/social-x.svg";
+import SocialInsta from "@/assets/social-insta.svg";
+import SocialLinkedIn from "@/assets/social-linkedin.svg";
+import SocialPin from "@/assets/social-pin.svg";
+import SocialYoutube from "@/assets/social-youtube.svg";
+import Image from "next/image";
+import Link from "next/link";
+
 export const Footer = () => {
-  return null;
+  return (
+    <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
+      <div className="container">
+        <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:w-full before:bg-[linear-gradient(to_right,#f87bff,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute">
+          <Image src={logo} height={40} alt="SaaS logo" className="relative" />
+        </div>
+        <nav>
+          <Link href="">About</Link>
+          <Link href="">Features</Link>
+          <Link href="">Customers</Link>
+          <Link href="">Pricing</Link>
+          <Link href="">Help</Link>
+          <Link href="">Careers</Link>
+        </nav>
+        <div className="">
+          <SocialX />
+          <SocialInsta />
+          <SocialLinkedIn />
+          <SocialPin />
+          <SocialYoutube />
+        </div>
+        <p>&copu; 2024 Your Company, Inc. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
