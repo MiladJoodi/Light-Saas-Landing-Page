1f31851573edc51b672d66f8816b124e57c4e9d7
Milad Joodi <winbeta.com@gmail.com>
2024-08-18 18:04:24 +0330

    up

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -55,7 +55,7 @@ const pricingTiers = [
 
 export const Pricing = () => {
   return (
-    <section className="py-24">
+    <section className="py-24 bg-white">
       <div className="container">
         <div className="section-heading">
           <h2 className="section-title">Pricing</h2>
@@ -76,7 +76,7 @@ export const Pricing = () => {
             }) => (
               <div
                 className={twMerge(
-                  "p-10 border border-[#F1F1F1] max-w-xs w-full rounded-3xl shadow-[0_7px_14px_#EAEAEA]",
+                  "card",
                   inverse === true && "border-black bg-black text-white"
                 )}
                 key={title}
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -7,6 +7,8 @@ import avatar6 from "@/assets/avatar-6.png";
 import avatar7 from "@/assets/avatar-7.png";
 import avatar8 from "@/assets/avatar-8.png";
 import avatar9 from "@/assets/avatar-9.png";
+import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
 const testimonials = [
   {
@@ -65,14 +67,68 @@ const testimonials = [
   },
 ];
 
-export const Testimonials = () => {
-  return <section>
-    <div className="container">
-      <div className="flex justify-center">
-        <div className="tag">Testimonials</div>
+const firstColumn = testimonials.slice(0, 3);
+const secondColumn = testimonials.slice(3, 6);
+const thirdColumn = testimonials.slice(6, 9);
+
+const TestimonialsColumn = (props: {
+  className?: string;
+  testimonials: typeof testimonials;
+}) => (
+  <div
+    className={twMerge(
+      "flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]",
+      props.className
+    )}
+  >
+    {props.testimonials.map(({ text, imageSrc, name, username }) => (
+      <div className="card" key={text}>
+        <div>{text}</div>
+        <div className="flex items-center gap-2 mt-5">
+          <Image
+            src={imageSrc}
+            alt={name}
+            width={40}
+            height={40}
+            className="h-10 w-10 rounded-full"
+          />
+          <div className="flex flex-col">
+            <div className="font-medium tracking-tight leading-5">{name}</div>
+            <div className="leading-5 tracking-tight">{username}</div>
+          </div>
+        </div>
       </div>
-      <h2 className="section-title">What our users say</h2>
-      <p className="section-description">From intutive design to powerful features, our app has become an essential tool for users around the world.</p>
-    </div>
-  </section>;
+    ))}
+  </div>
+);
+
+export const Testimonials = () => {
+  return (
+    <section className="bg-white">
+      <div className="container">
+        <div className="section-heading">
+          <div className="flex justify-center">
+            <div className="tag">Testimonials</div>
+          </div>
+          <h2 className="section-title mt-5">What our users say</h2>
+          <p className="section-description">
+            From intutive design to powerful features, our app has become an
+            essential tool for users around the world.
+          </p>
+        </div>
+
+        <div className="flex justify-center gap-6">
+          <TestimonialsColumn testimonials={firstColumn} />
+          <TestimonialsColumn
+            testimonials={secondColumn}
+            className="hidden md:flex"
+          />
+          <TestimonialsColumn
+            testimonials={thirdColumn}
+            className="hidden lg:flex"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
