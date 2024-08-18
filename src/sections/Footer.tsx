0b76f83a2b4c72d1b16a47047e5ff8531b15cea9
Milad Joodi <winbeta.com@gmail.com>
2024-08-18 21:26:26 +0330

    up

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -11,10 +11,10 @@ export const Footer = () => {
   return (
     <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
       <div className="container">
-        <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:w-full before:bg-[linear-gradient(to_right,#f87bff,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute">
+        <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:w-full before:blur before:bg-[linear-gradient(to_right,#f87bff,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute">
           <Image src={logo} height={40} alt="SaaS logo" className="relative" />
         </div>
-        <nav>
+        <nav className="flex flex-col md:flex-row md:justify-center gap-6 mt-6">
           <Link href="">About</Link>
           <Link href="">Features</Link>
           <Link href="">Customers</Link>
@@ -22,14 +22,14 @@ export const Footer = () => {
           <Link href="">Help</Link>
           <Link href="">Careers</Link>
         </nav>
-        <div className="">
+        <div className="flex justify-center gap-6 mt-6">
           <SocialX />
           <SocialInsta />
           <SocialLinkedIn />
           <SocialPin />
           <SocialYoutube />
         </div>
-        <p>&copu; 2024 Your Company, Inc. All rights reserved.</p>
+        <p className="mt-6">&copy; 2024 Your Company, Inc. All rights reserved.</p>
       </div>
     </footer>
   );
